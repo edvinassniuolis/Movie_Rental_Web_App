ae21e48c1dc1c2257a674d77c0ea737cae89230e
Edvinas <edvinas.sniuolis@gmail.com>
2017-09-30 20:54:47 +0300

    Add project files.

---
+++ Movie_Renta_Web_App/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Movie_Renta_Web_App.Startup))]
+namespace Movie_Renta_Web_App
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
